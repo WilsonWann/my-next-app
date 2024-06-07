8e37215075823b8471d29088c1101577d9dc798f
DESKTOP-DO30QMK\User <genius19880825@hotmail.com>
2024-06-07 15:40:37 +0800

    Refactor home page animation and layout
    Refactor the home page to utilize useAnimation and useInView hooks for animating elements based on their visibility in the viewport. This improves the user experience by adding subtle animations while scrolling through the page. The layout adjustments also enhance the responsiveness and visual appeal of the page. No issues associated.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,36 +3,46 @@ import { useEffect } from "react";
 import slicedOranges from "@/assets/sliced-oranges.jpg";
 import orange from "@/assets/small-orange-pumpkin-with-green-leaves-behind.jpg";
 import orangePile from "@/assets/fresh-oranges-pile.jpg";
-import orangeInGrass from "@/assets/orange-in-grass.jpg";
 import anOrangeBasket from "@/assets/an-orange-basket.jpg";
-import grass from "@/assets/photo-in-macro-of-wet-green-grass.jpg";
 import paintingFrame from "@/assets/wet-orange-and-white-paint-filling-frame.jpg";
 import Carousel from "./components/Carousel/Carousel.component";
 import Image from "next/image";
-import { motion, useAnimation, useScroll, useTransform } from "framer-motion"
+import { motion, useAnimation } from "framer-motion"
 import { useInView } from 'react-intersection-observer';
 
 const bannerList = [slicedOranges, orange, orangePile];
 
 export default function Home() {
 
-  // const { scrollYProgress } = useScroll();
-  // const x = useTransform(scrollYProgress, [0, 1], [-200, 0]);
-  // const opacity = useTransform(scrollYProgress, [0, 1], [0, 1]);
-
-  const controls = useAnimation();
-  const [ref, inView] = useInView({
+  const controlsAbout = useAnimation();
+  const [refAbout, aboutInView] = useInView({
     triggerOnce: false, // 每次進入視口時都觸發
     threshold: 0.1, // 只有10%可見時就觸發
   });
 
   useEffect(() => {
-    if (inView) {
-      controls.start({ x: 0, opacity: 1 });
+    if (aboutInView) {
+      controlsAbout.start({ x: 0, opacity: 1 });
     } else {
-      controls.start({ x: 100, opacity: 0 });
+      controlsAbout.start({ x: 100, opacity: 0 });
     }
-  }, [controls, inView]);
+  }, [controlsAbout, aboutInView]);
+
+  const controlsCompany = useAnimation();
+  const [refCompany, companyInView] = useInView({
+    triggerOnce: false, // 每次進入視口時都觸發
+    threshold: 0.1, // 只有10%可見時就觸發
+  });
+
+  useEffect(() => {
+    if (companyInView) {
+      controlsCompany.start({ x: 0, opacity: 1 });
+    } else {
+      controlsCompany.start({ x: -100, opacity: 0 });
+    }
+  }, [controlsCompany, companyInView]);
+
+
 
   return (
     <div className="direct-mb-32 flex flex-col items-center justify-between">
@@ -42,8 +52,8 @@ export default function Home() {
       </div>
       {/* //* left: image */}
       {/* //* right: text: to left fade-in */}
-      <div id="about" className="relative flex w-full flex-row items-start justify-start gap-32">
-        <div className="relative h-screen basis-[1066px]">
+      <div id="about" className="relative flex w-full h-screen flex-row items-start justify-start gap-32">
+        <div className="relative h-screen basis-[1076px]">
           <div className="absolute h-full w-full">
             <Image
               src={paintingFrame}
@@ -51,7 +61,7 @@ export default function Home() {
               blurDataURL={paintingFrame.blurDataURL}
               alt={""}
               sizes="100vw"
-              className="absolute left-0 top-14 z-10 h-[617px] w-[1010px] object-cover object-center"
+              className="absolute left-0 top-24 z-10 h-[671px] w-[1010px] object-cover object-center"
             />
             <Image
               src={anOrangeBasket}
@@ -59,14 +69,14 @@ export default function Home() {
               blurDataURL={anOrangeBasket.blurDataURL}
               alt={""}
               sizes="100vw"
-              className="absolute left-14 z-20 h-[617px] w-[1010px] object-cover object-top"
+              className="absolute left-16 z-20 h-[671px] w-[1010px] object-cover object-top"
             />
           </div>
         </div>
         <div className="pt-48">
           <motion.div
-            ref={ref}
-            animate={controls}
+            ref={refAbout}
+            animate={controlsAbout}
             transition={{ duration: 0.8, ease: "easeOut" }}
           >
             <section className="item-start flex flex-col gap-8">
@@ -87,7 +97,7 @@ export default function Home() {
                   我們的策略助力您的願景實現無限成長。
                 </pre>
               </div>
-              <div data-attr="＊" className="before:content-[attr(data-attr)]">
+              <div data-attr="﹡" className="text-lg before:content-[attr(data-attr)]">
                 musense
               </div>
             </section>
@@ -96,9 +106,23 @@ export default function Home() {
       </div>
       {/* //* left: text: to right fade-in */}
       {/* //* right: text */}
-      <div>
-        <div></div>
-        <div></div>
+      <div className="w-full h-screen flex justify-center items-center">
+        <div className="flex flex-row justify-center items-end gap-4">
+          <motion.div
+            ref={refCompany}
+            animate={controlsCompany}
+            transition={{ duration: 0.8, ease: "easeOut" }}
+          >
+
+            <h2 className="text-7xl font-thin">陌聲行銷</h2>
+          </motion.div>
+          <h2 className="text-5xl font-light">﹡</h2>
+
+          <div className="flex flex-col justify-start items-baseline">
+            <div className="text-xl font-bold">數字行銷，智能決策，創意無限</div>
+            <div className="text-lg font-normal">數據驅動，智能行銷，創意無限，助力品牌實現精準決策與卓越成效。</div>
+          </div>
+        </div>
       </div>
       {/* //* one: image: to up fade-in */}
       {/* //* two: image: to down fade-in */}
