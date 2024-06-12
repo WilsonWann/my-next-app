e99eb9dbd762785d1eb455bab9a72d6436f5f6b8
DESKTOP-DO30QMK\User <genius19880825@hotmail.com>
2024-06-12 17:39:53 +0800

    Refactor image handling and import in page.tsx
    Consolidate image imports into a separate file for reusability.
    
    #Refactor #CodeOrganization

---
+++ src/app/imageHandler.ts
@@ -0,0 +1,20 @@
+import stackedBracelets from "@/assets/stacked-bracelets.jpg";
+import weddingVowsAndRings from "@/assets/wedding-vows-and-wedding-rings.jpg";
+import holdingHands from "@/assets/holding-hands.jpg";
+
+import womansNecklace from "@/assets/womans-necklace.jpg";
+import detailedSnowflake from "@/assets/detailed-snowflake.jpg";
+
+import moonBracelet from "@/assets/womens-hand-moon-bracelet.jpg";
+import goldChainNecklace from "@/assets/gold-chain-necklace-on-corner.jpg";
+import youngProfessional from "@/assets/young-professional.jpg";
+import galaxyEarrings from "@/assets/galaxy-earrings.jpg";
+
+import modernJewelry from "@/assets/modern-jewelry-and-accessories-on-display.jpg";
+
+const bannerList = [stackedBracelets, weddingVowsAndRings, holdingHands];
+const aboutImages = [detailedSnowflake, womansNecklace];
+const photosImages = [moonBracelet, goldChainNecklace, youngProfessional, galaxyEarrings];
+const largeBannerImage = modernJewelry;
+
+export { bannerList, aboutImages, photosImages, largeBannerImage }
