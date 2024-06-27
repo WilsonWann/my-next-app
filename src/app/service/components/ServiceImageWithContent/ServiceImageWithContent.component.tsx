adb12e3226d163416265492d0026a251edab2e46
DESKTOP-DO30QMK\User <genius19880825@hotmail.com>
2024-06-27 17:30:17 +0800

    add animation to service page

--- src/app/service/components/ServiceImageWithContent/ServiceImageWithContent.component.tsx
+++ src/app/service/components/ServiceImageWithContent/ServiceImageWithContent.component.tsx
@@ -1,7 +1,12 @@
-import React, { FC } from 'react'
+'use client'
+
+import React, { FC, useEffect } from 'react'
 import { noto } from '@/app/fonts'
 import Image, { type StaticImageData } from 'next/image'
 import { Separator } from '@/components/ui/separator'
+import { motion, useAnimation } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+
 
 type ServiceProps = {
   image: StaticImageData
@@ -11,6 +16,20 @@ type ServiceProps = {
   content: string
 }
 const ServiceImageWithContent: FC<ServiceProps> = ({ image, reverse = false, order, title, content }) => {
+
+  const controlsContent = useAnimation();
+  const [refContent, contentInView] = useInView({
+    triggerOnce: false,
+    threshold: 0.1,
+  });
+
+  useEffect(() => {
+    if (contentInView) {
+      controlsContent.start({ x: 0, opacity: 1 });
+    } else {
+      controlsContent.start({ x: reverse ? -100 : 100, opacity: 0 });
+    }
+  }, [controlsContent, contentInView, reverse]);
 
   const imageProps = {
     src: image,
@@ -32,11 +51,17 @@ const ServiceImageWithContent: FC<ServiceProps> = ({ image, reverse = false, ord
         <Image {...imageProps} fill className="object-cover object-center" />
       </div>
       <div className="flex-grow md:flex-shrink-0 xl:basis-1/2 md:basis-5/12  flex flex-col items-center justify-center">
-        <div className="flex flex-col justify-center items-start gap-6 w-fit">
-          <h2 className="text-title font-bold text-3xl">{`0${order}. ${title}`}</h2>
-          <pre className={`${noto.className} leading-8 text-base tracking-widest`}>{content}</pre>
-          <Separator className="bg-title w-12 h-1" />
-        </div>
+        <motion.div
+          ref={refContent}
+          animate={controlsContent}
+          transition={{ duration: 0.8, ease: "easeOut" }}
+        >
+          <div className="flex flex-col justify-center items-start gap-6 w-fit">
+            <h2 className="text-title font-bold text-3xl">{`0${order}. ${title}`}</h2>
+            <pre className={`${noto.className} leading-8 text-base tracking-widest`}>{content}</pre>
+            <Separator className="bg-title w-12 h-1" />
+          </div>
+        </motion.div>
       </div>
     </div>
   )
