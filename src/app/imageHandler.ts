87f151a75d28559edd14ac9bd6d65ab03d02987c
DESKTOP-DO30QMK\User <genius19880825@hotmail.com>
2024-06-27 15:42:48 +0800

    content rewrite and images replace

--- src/app/imageHandler.ts
+++ src/app/imageHandler.ts
@@ -16,6 +16,16 @@ import modernWatch from "@/assets/modern-jewelry-and-accessories-on-display.jpg"
 import makeupAndJewelry from "@/assets/makeup-and-jewelry.jpg";
 import gemStonesAndToolsFlatlay from "@/assets/gemstones-and-tools-flatlay.jpg";
 import earringsInALine from "@/assets/blue-and-pink-background-with-earrings-in-a-line.jpg";
+
+import businessWomenWorking from "@/assets/business-women-working.jpg"
+import personWorkingOnFashionDesignOnATablet from "@/assets/person-working-on-fashion-design-on-a-tablet.jpg"
+import gemstonesAndToolsFlatlay from "@/assets/gemstones-and-tools-flatlay.jpg"
+import officeTeamBusinessMeeting from "@/assets/office-team-business-meeting.jpg"
+import jewelersWorkDesk from "@/assets/jewelers-work-desk.jpg"
+import preparingToFinishARing from "@/assets/preparing-to-finish-a-ring.jpg"
+import goldRingWithWiteDiamondsAndALargeStone from "@/assets/gold-ring-with-white-diamonds-and-a-large-stone.jpg"
+import professionalSmilingWoman from "@/assets/professional-smiling-woman.jpg"
+
 import { StaticImageData } from "next/image";
 
 const bannerList = [stackedBracelets, weddingVowsAndRings, holdingHands];
@@ -26,56 +36,56 @@ const mainBannerImage = modernWatch;
 const serviceBannerImage = makeupAndJewelry
 const serviceContentImages = [
   {
-    image: moonBracelet,
+    image: businessWomenWorking,
     title: '初步諮詢',
     content: `了解客戶的需求與喜好
 解說設計流程及細節
 設計費、付款方式說明`
   },
   {
-    image: goldChainNecklace,
+    image: personWorkingOnFashionDesignOnATablet,
     title: '設計概念',
     content: `根據客戶需求和風格進行設計草圖
 提供設計概念與建議
 確認設計方向`
   },
   {
-    image: youngProfessional,
+    image: gemstonesAndToolsFlatlay,
     title: '選材搭配',
     content: `根據設計草圖選擇合適的寶石和金屬材料
 提供不同材質的搭配建議
 確認最終設計方案`
   },
   {
-    image: galaxyEarrings,
+    image: officeTeamBusinessMeeting,
     title: '設計合約',
     content: `簽訂設計合約並收取設計費
 繪製詳細設計圖紙
 確認製作細節和時間表`
   },
   {
-    image: moonBracelet,
+    image: jewelersWorkDesk,
     title: '工藝製作',
     content: `開始進行首飾製作
 按照設計圖紙精雕細琢
 定期向客戶報告進度`
   },
   {
-    image: goldChainNecklace,
+    image: preparingToFinishARing,
     title: '品質檢驗',
     content: `完成製作後進行質量檢查
 確保每件首飾符合高品質標準
 進行最後的細節修正`
   },
   {
-    image: youngProfessional,
+    image: goldRingWithWiteDiamondsAndALargeStone,
     title: '交付首飾',
     content: `與客戶一起檢查完成的首飾
 確認滿意後進行交付
 收取最終付款`
   },
   {
-    image: galaxyEarrings,
+    image: professionalSmilingWoman,
     title: '售後服務',
     content: `提供完善的售後服務及保養建議
 免費諮詢及維修服務
