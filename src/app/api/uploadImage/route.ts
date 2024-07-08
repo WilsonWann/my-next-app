b68534994211dd655ebbc76f250c12fb24b5b0bd
DESKTOP-DO30QMK\User <genius19880825@hotmail.com>
2024-07-08 17:14:15 +0800

    remove deprecated export config

--- src/app/api/uploadImage/route.ts
+++ src/app/api/uploadImage/route.ts
@@ -1,12 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import uploadImage from '@/lib/aws/uploadImage';
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
 export async function POST(req: NextRequest) {
 
   const formData = await req.formData();
