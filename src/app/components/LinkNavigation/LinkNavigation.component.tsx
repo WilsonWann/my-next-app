4bcf013072f2b3540c1a6585ced03d233f793305
DESKTOP-DO30QMK\User <genius19880825@hotmail.com>
2024-07-09 10:15:02 +0800

    修改video中文

--- src/app/components/LinkNavigation/LinkNavigation.component.tsx
+++ src/app/components/LinkNavigation/LinkNavigation.component.tsx
@@ -14,7 +14,7 @@ const routeDetailArray: RouteDetail[] = [
   { route: '/', main_name: 'about', alt_name: '關於陌聲', onClick: () => handleScrollTo('about') },
   { route: '/service', main_name: 'service', alt_name: '服務內容' },
   { route: '/portfolio', main_name: 'portfolio', alt_name: '作品集' },
-  { route: '/video', main_name: 'video', alt_name: '案例分享' },
+  { route: '/video', main_name: 'video', alt_name: '影音分享' },
   { route: '/contact', main_name: 'contact', alt_name: '聯絡我們' },
 ]
 
