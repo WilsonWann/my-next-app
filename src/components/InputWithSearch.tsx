4d98a3df303753c40a0d507eca60c21fc47b6edd
DESKTOP-DO30QMK\User <genius19880825@hotmail.com>
2024-07-09 17:19:46 +0800

    introduce InputWithSearch to modularize the component

---
+++ src/components/InputWithSearch.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import React, { forwardRef } from "react"
+import { Input } from "@/components/ui/input"
+import { Search } from "lucide-react"
+
+type Props = {
+  onSearchClick: () => void
+}
+
+const InputWithSearch = forwardRef<HTMLInputElement, Props>(
+  (props, ref) => {
+
+    const { onSearchClick } = props
+
+    return (
+      <div className="flex w-full max-w-sm items-center space-x-2">
+        <Input ref={ref} type="search" placeholder="search" className="pr-10" />
+        <Search className="!-ml-8 cursor-pointer" onClick={onSearchClick} />
+      </div>
+    )
+  }
+)
+
+export default InputWithSearch
