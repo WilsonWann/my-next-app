184fc435e6db1ffec9c4843bb9935403c90c7484
DESKTOP-DO30QMK\User <genius19880825@hotmail.com>
2024-07-12 16:17:58 +0800

    add generateMetadata for [portfolioCase] route

--- src/app/portfolio/[portfolioCase]/page.tsx
+++ src/app/portfolio/[portfolioCase]/page.tsx
@@ -1,6 +1,63 @@
+import { type Metadata } from 'next'
 import React, { FC, Suspense } from 'react'
 import Loading from './loading'
 import PortfolioCase from './components/PortfolioCase/PortfolioCase.component'
+import { PORTFOLIO_CASES } from '@/lib/constants'
+
+const defaultTitle = "作品集 – rings | necklace | jewelry | bracelet | earrings | watch"
+const defaultDescription = `珠寶設計與生產首選陌聲行銷！陌聲提供專業珠寶設計、珠寶生產、珠寶保養、珠寶課程等服務，致力於傾聽客戶需求、展現精湛工藝和卓越品質。`
+
+const defaultOgImageObj = {
+  alt: defaultTitle,
+  url: '/portfolio/jewelers-work-desk.jpg',
+  width: 1200,
+  height: 630
+}
+
+export async function generateMetadata({ params: { portfolioCase } }: PortfolioCaseParamsProps): Promise<Metadata> {
+
+
+  const portfolioCaseItem = PORTFOLIO_CASES.find(item => item.name === portfolioCase)
+
+  if (!portfolioCaseItem) {
+
+    return {
+      title: defaultTitle,
+      openGraph: {
+        type: "website",
+        title: defaultTitle,
+        description: defaultDescription,
+        siteName: "Musense Jewelry Design",
+        images: [defaultOgImageObj],
+      },
+      twitter: {
+        images: [defaultOgImageObj]
+      },
+    }
+  }
+  const { title, description, imageUrl } = portfolioCaseItem
+
+  const ogImageObj = {
+    alt: title,
+    url: imageUrl,
+    width: 1200,
+    height: 630
+  }
+
+  return {
+    title,
+    openGraph: {
+      type: "website",
+      title,
+      description,
+      siteName: "Musense Jewelry Design",
+      images: [ogImageObj],
+    },
+    twitter: {
+      images: [ogImageObj]
+    },
+  }
+}
 
 export const maxDuration = 60
 
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,42 @@
+import { Description } from "@radix-ui/react-dialog";
+
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
+
+export const PORTFOLIO_CASES = [
+  {
+    name: "rings",
+    imageUrl: '/portfolio/rings/wedding-vows-and-wedding-rings.jpg',
+    title: "VOWS | RINGS",
+    description: "鑽戒, 婚禮, RINGS"
+  },
+  {
+    name: "necklace",
+    imageUrl: '/portfolio/necklace/wedding-vows-and-wedding-rings.jpg',
+    title: "GOLD CHAIN | NECKLACE",
+    description: "鑽戒, 婚禮, RINGS"
+  },
+  {
+    name: "jewelry",
+    imageUrl: '/portfolio/jewelry/gemstones-and-tools-flatlay.jpg',
+    title: "GEMSTONES | JEWELRY",
+    description: "珠寶, 玉石, JEWELRY"
+  },
+  {
+    name: "bracelet",
+    imageUrl: '/portfolio/bracelet/womens-hand-moon-bracelet.jpg',
+    title: "LEISURE | BRACELET",
+    description: "手環, 個性, BRACELET"
+  },
+  {
+    name: "earrings",
+    imageUrl: '/portfolio/earrings/blue-and-pink-background-with-earrings-in-a-line.jpg',
+    title: "MODERN | EARRINGS",
+    description: "耳環, 場合, EARRINGS"
+  },
+  {
+    name: "watch",
+    imageUrl: '/portfolio/watch/modern-jewelry-and-accessories-on-display.jpg',
+    title: "VOWS | WATCH",
+    description: "手錶, 形象, WATCH"
+  },
+]
